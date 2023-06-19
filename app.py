7d5281f731b54d81d61be5d2c1c5c8952bda1083
Ved Vekhande <202111087@diu.iiitvadodara.ac.in>
2023-06-19 19:01:31 +0530

    updated

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from llama_index import VectorStoreIndex, SimpleDirectoryReader
 
 app = Flask(__name__)
 
-# Load documents and create the index
 documents = SimpleDirectoryReader(r'D:\NLP\practice\data').load_data()
 index = VectorStoreIndex.from_documents(documents)
 
@@ -15,11 +14,9 @@ def home():
 def chat():
     user_input = request.form['user_input']
 
-    # Query the index with the user input
     query_engine = index.as_query_engine()
     response = query_engine.query(user_input)
 
-    # Convert the response to a string
     bot_response = str(response)
 
     return bot_response
