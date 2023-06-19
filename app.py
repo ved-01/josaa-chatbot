714daae17f20a5e02b0b9de9b6d4959f8cb12839
Ved Vekhande <202111087@diu.iiitvadodara.ac.in>
2023-06-19 17:11:19 +0530

    added files

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from llama_index import VectorStoreIndex, SimpleDirectoryReader
+
+app = Flask(__name__)
+
+# Load documents and create the index
+documents = SimpleDirectoryReader(r'D:\NLP\practice\data').load_data()
+index = VectorStoreIndex.from_documents(documents)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_input = request.form['user_input']
+
+    # Query the index with the user input
+    query_engine = index.as_query_engine()
+    response = query_engine.query(user_input)
+
+    # Convert the response to a string
+    bot_response = ', '.join(response)
+
+    return bot_response
+
+if __name__ == '__main__':
+    app.run(debug=True)
