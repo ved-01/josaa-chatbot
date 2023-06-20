a734032af6ac3235c28cbf74e5c3e34abd5867af
Ved Vekhande <202111087@diu.iiitvadodara.ac.in>
2023-06-20 13:39:15 +0530

    minor

--- static/script.js
+++ static/script.js
@@ -1,46 +1,37 @@
-// Get HTML elements
+
 var chatLog = document.getElementById('chat-log');
 var userInput = document.getElementById('user-input');
 var sendBtn = document.getElementById('send-btn');
 
-// Event listener for send button click
 sendBtn.addEventListener('click', function() {
     sendMessage();
 });
 
-// Event listener for Enter key press in user input
 userInput.addEventListener('keydown', function(event) {
     if (event.keyCode === 13) {
         sendMessage();
     }
 });
 
-// Function to send user message and get bot response
 function sendMessage() {
     var userMessage = userInput.value;
 
-    // Append user message to chat log
     appendMessage('user', userMessage);
 
-    // Clear user input
     userInput.value = '';
 
-    // Send user message to backend for processing
     sendRequest(userMessage);
 }
 
-// Function to append message to chat log
 function appendMessage(sender, message) {
     var messageElement = document.createElement('div');
     messageElement.classList.add(sender + '-message');
     messageElement.innerHTML = message;
     chatLog.appendChild(messageElement);
 
-    // Scroll chat log to bottom
     chatLog.scrollTop = chatLog.scrollHeight;
 }
 
-// Function to send user message to backend and receive bot response
 function sendRequest(userMessage) {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '/chat', true);
